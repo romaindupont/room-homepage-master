129dbe35ecf05b3586e4d3c6f6000c903ca5dc0f
romaindupont <rd.romaindupont@free.fr>
2020-12-06 21:07:25 +0100

    essai desktop

--- js/app.js
+++ js/app.js
@@ -11,17 +11,26 @@ getText: function(event){
     event.preventDefault();
      if(document.getElementById('first-container').style.display != "none"){
         document.getElementById('first-container').style.display = "none";
+        document.getElementById('image-mobile1').style.display = "none";
         document.getElementById('second-container').style.display='block';
+        document.getElementById('image-mobile2').style.display = "block";        
         document.getElementById('third-container').style.display = "none";
+        document.getElementById('image-mobile3').style.display = "none";
     }
     else if(document.getElementById('second-container').style.display != "none"){
         document.getElementById('first-container').style.display='none';
+        document.getElementById('image-mobile1').style.display = "none";
         document.getElementById('second-container').style.display='none';
+        document.getElementById('image-mobile2').style.display = "none";
         document.getElementById('third-container').style.display = "block";
+        document.getElementById('image-mobile3').style.display = "block";
     } else if (document.getElementById('third-container').style.display != "none"){
         document.getElementById('first-container').style.display='block';
+        document.getElementById('image-mobile1').style.display = "block";
         document.getElementById('second-container').style.display='none';
+        document.getElementById('image-mobile2').style.display = "none";
         document.getElementById('third-container').style.display = "none";
+        document.getElementById('image-mobile3').style.display = "none";
     }
     }
    /*  document.getElementById('first-container').style.display = 'none';
